766e52b7c47db1bf4f98c3ab7ba3a1cfe09b5616
Annisa Nur Pratiwi <129244088+annisanp373@users.noreply.github.com>
2024-07-25 15:35:55 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,18 @@
 
 const express = require('express');
 const serverless = require('serverless-http');
-const router = express.Router();
-const { sequelize } = require('../models/index'); // Import Sequelize instance dan model
+const app = express();
+const router = require('./'); // Import routes dari folder routes
+const { sequelize } = require('../models'); // Import Sequelize instance dan model
 
 // Middleware untuk parsing JSON
-router.use(express.json());
-
-// Rute utama
-router.get('/', function(req, res, next) {
-  res.json({ message: 'Welcome to Express' });
-});
+app.use(express.json());
+app.use('/', router); // Gunakan router untuk menangani rute
 
 // Sinkronkan database ketika aplikasi dimulai
 sequelize.sync()
   .then(() => console.log('Database synchronized with Sequelize'))
   .catch(err => console.error('Error synchronizing database with Sequelize:', err));
-
-// Buat server Express dan gunakan router
-const app = express();
-app.use('/', router);
 
 // Export handler untuk serverless function
 module.exports.handler = serverless(app);
